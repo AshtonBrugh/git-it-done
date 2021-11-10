53d32e95decfb8880cbb59d710fb89c025cb41a3
Ashton B <ashtonbrugh@gmail.com>
2021-11-10 10:56:10 -0500

    adding a message for 30+ repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
 
@@ -11,6 +11,11 @@ fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             //pass response data to dom function
             displayIssues(data);
+              
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
         });
     }
     else {
@@ -57,5 +62,18 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
     }
 };
+
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on Github.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to warning container
+    limitWarningEl.appendChild(linkEl);
+}
 
 getRepoIssues("facebook/react");
